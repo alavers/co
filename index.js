a7ab41ef0065b89ed7fef21b76f0afcd948648b8
Andrew Lavers <andrew@smooch.io>
2018-05-07 20:29:15 -0400

    Restore arrayToPromise and put a warning instead

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function toPromise(obj) {
   if (isPromise(obj)) return obj;
   if (isGeneratorFunction(obj) || isGenerator(obj)) return co.call(this, obj);
   if ('function' == typeof obj) return thunkToPromise.call(this, obj);
-  if (Array.isArray(obj)) throw new Error('Attempted to yield an array. You must use Promise.all');
+  if (Array.isArray(obj)) return arrayToPromise.call(this, obj);
   if (isObject(obj)) return objectToPromise.call(this, obj);
   return obj;
 }
@@ -140,6 +140,21 @@ function thunkToPromise(fn) {
       resolve(res);
     });
   });
+}
+
+/**
+ * Convert an array of "yieldables" to a promise.
+ * Uses `Promise.all()` internally.
+ *
+ * @param {Array} obj
+ * @return {Promise}
+ * @api private
+ */
+
+function arrayToPromise(obj) {
+  const err = new Error();
+  console.error('WARNING: yield [] is unsafe', err.stack);
+  return Promise.all(obj.map(toPromise, this));
 }
 
 /**
